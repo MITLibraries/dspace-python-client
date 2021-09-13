12eca6ea751f15ac361067fcafd0c22e02fbe0a8
Helen Bailey <hbailey@mit.edu>
2021-09-13 17:08:38 -0400

    Prepare package for initial pre-release
    Why these changes are being introduced:
    A few minor changes were needed to build and distribute the package
    successfully on Github.
    
    How this addresses that need:
    * Updates pyproject.toml to specify package name
    * Updates package __init__ file to declare imports
    * Adds a build command to the Makefile
    * Updates dependencies and python version
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/ETD-433

--- dspace/__init__.py
+++ dspace/__init__.py
@@ -1,5 +1,9 @@
 # dspace/__init__.py
-"""DSpace package."""
+"""DSpace Python Client package."""
 import logging
+
+from dspace.bitstream import Bitstream
+from dspace.client import DSpaceClient
+from dspace.item import Item, MetadataEntry
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
